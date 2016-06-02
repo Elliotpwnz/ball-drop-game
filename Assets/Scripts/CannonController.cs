fd312b8f942bb00e0ace8d379670cf061f349b6f
Elliot Simpson <Elliotsminion@gmail.com>
2016-06-02 11:21:36 -0400

    cannon basic functionality

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonController : MonoBehaviour {
+
+	public float RotationSpeed;
+	private Vector3 dir;
+	public float ballSpeed;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate((Input.GetAxis("Mouse Y") * -RotationSpeed * Time.deltaTime), (Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime), 0, Space.World);
+	
+		if (Input.GetMouseButtonDown (0)) {
+			dir = transform.forward;
+			GameObject cannonBall = Instantiate (Resources.Load ("Cannonball")) as GameObject;
+			Rigidbody rb = cannonBall.GetComponent<Rigidbody> ();
+			cannonBall.transform.position = transform.position;
+			rb.AddForce (dir * ballSpeed);
+		}
+	}
+}
---
+++ Assets/Scripts/CannonballController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonballController : MonoBehaviour {
+	private Rigidbody rb;
+	private Vector3 dir;
+	public float ballSpeed;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = Color.black;
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			dir = GameObject.Find ("Cannon").gameObject.transform.forward;
+			rb.AddForce (dir * ballSpeed);
+		}
+	}
+}
