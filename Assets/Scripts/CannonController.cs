47dec677f15265e8e928d80cb15483c9a27e24c7
Elliot Simpson <Elliotsminion@gmail.com>
2016-06-02 11:55:33 -0400

    bug fix

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -18,6 +18,7 @@ public class CannonController : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			dir = transform.forward;
 			GameObject cannonBall = Instantiate (Resources.Load ("Cannonball")) as GameObject;
+			cannonBall.GetComponent<Renderer> ().material.color = Color.black;
 			Rigidbody rb = cannonBall.GetComponent<Rigidbody> ();
 			cannonBall.transform.position = transform.position;
 			rb.AddForce (dir * ballSpeed);
