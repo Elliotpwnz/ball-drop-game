e73bee62329d40272015cc30d76ce3ff85ace428
Elliot Simpson <Elliotsminion@gmail.com>
2016-06-03 20:51:44 -0400

    updates

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -13,8 +13,15 @@ public class CannonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		//if (moveHorizontal != 0 || moveVertical != 0) {
+		//	Vector3 cannonRotation = new Vector3 (moveVertical * -RotationSpeed * Time.deltaTime, moveHorizontal * RotationSpeed * Time.deltaTime, 0.0f);
+		//	transform.Rotate (cannonRotation);
+		//}
+
 		transform.Rotate((Input.GetAxis("Mouse Y") * -RotationSpeed * Time.deltaTime), (Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime), 0, Space.World);
-	
+
 		if (Input.GetMouseButtonDown (0)) {
 			dir = transform.forward;
 			GameObject cannonBall = Instantiate (Resources.Load ("Cannonball")) as GameObject;
---
+++ Assets/Scripts/warriorController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class warriorController : MonoBehaviour {
+
+	public float enemySpeed;
+	private float step;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.color = Color.yellow;
+		step = enemySpeed * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, GameObject.Find ("Cannon").transform.position, step);
+	}
+}
