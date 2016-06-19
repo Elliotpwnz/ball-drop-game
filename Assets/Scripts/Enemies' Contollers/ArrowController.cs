b8b9663cf176c456da13e7b7b37b5f0894167f36
Mohammed Jalal Hemidach <Mohammed Jalal Hemidach>
2016-06-19 19:52:52 -0400

    Created Arrow prefab, Arrow script, and made the archer shoots toward the castle

---
+++ Assets/Scripts/Enemies' Contollers/ArrowController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+
+	public float thrust;
+	private Transform archer;
+	private Rigidbody ballRigidBody;
+	// Use this for initialization
+	void Start () {
+		archer = GameObject.Find ("Archer").transform;
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		ballRigidBody = gameObject.GetComponent<Rigidbody> ();
+		Vector3 aimingDirection = - archer.forward;
+		ballRigidBody.AddForce (aimingDirection * thrust);
+	}
+
+	void FixedUpdate(){
+		//transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0.0f);
+		//ballRigidBody.AddForce (speed);
+		//transform.Rotate((Input.GetAxis("Mouse Y") * rotator * RotationSpeed * Time.deltaTime), (Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime), 0, Space.World);
+		//transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0.0f);
+	}
+}
