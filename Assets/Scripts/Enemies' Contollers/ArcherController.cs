c86a88470d92bdd8e9d7fcd3e5c45f7a22636c6a
Mohammed Jalal Hemidach <Mohammed Jalal Hemidach>
2016-06-25 05:54:59 -0400

    Made the HealthBar controllable form its Parent object by passing an instance of the Healthbar to the parent

--- Assets/Scripts/Enemies' Contollers/ArcherController.cs
+++ Assets/Scripts/Enemies' Contollers/ArcherController.cs
@@ -2,41 +2,47 @@
 using System.Collections;
 
 public class ArcherController : MonoBehaviour {
-
-	public GameObject GreenHealthBar;
+	public GameObject HealthBar;
 	public Transform arrowSpawn;
 	public GameObject Arrow;
 	public float enemySpeed;
 	private float step;
 	private int fireRate = 5;
 	float nextFire;
-
-	public bool gotShot;
+	private Vector3 firstTransition, secondTransition;
 
 	// Use this for initialization
 	void Start () {
+		
 		GetComponent<Renderer> ().material.color = Color.green;
 		nextFire = Time.time;
+		//firstTransition = new Vector3 (5.0f * Time.deltaTime, 0.0f, 0.0f);
+		//secondTransition = new Vector3 (0.0f, 0.0f, 10.0f * Time.deltaTime);
+		//setupHealthBars();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		gotShot = false;
+		//Archer update
+		//transform.Translate (new Vector3(-0.5f, 0.0f, 0.0f));
+		//transform.Translate (transform.position + secondTransition);
+
 		if (Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			Instantiate (Arrow, arrowSpawn.position, arrowSpawn.rotation);
 
 		}
 
-		if (GreenHealthBar.GetComponent<GreenHealthBarController> ().currentHealth <= 0) {
+		if (HealthBar.GetComponent<HealthBarController>().greenCurrentHealth <= 0) {
 			Destroy (gameObject);
 		}
 	}
 
 	void OnCollisionEnter(Collision coll){
 		if (coll.gameObject.tag == "Ball") {
-			gotShot = true;
+			HealthBar.GetComponent<HealthBarController>().greenCurrentHealth -= 2;
+			HealthBar.GetComponent<HealthBarController>().redCurrentHealth += 2;
 		}
 	}
 }
---
+++ Assets/Scripts/Enemies' Contollers/HealthBarController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarController : MonoBehaviour {
+
+	//HealthBar control variables
+	public GameObject GreenHealthBar, RedHealthBar;
+	public int greenMaxHealth, redMaxHealth;
+	public int greenCurrentHealth, redCurrentHealth;
+	public float greenHealthPercent, redHealthPercent;
+	public float greenOldScaleX, redOldScaleX;
+	public Vector3 greenCurrentScale, redCurrentScale;
+	public float greenNewScaleX, redNewScaleX;
+	public Vector3 redPosition;
+
+	// Use this for initialization
+	void Start () {
+		//GreenHealthBar set up
+		GreenHealthBar.GetComponent<Renderer> ().material.color = Color.green;
+		greenCurrentHealth = greenMaxHealth;
+		greenOldScaleX = GreenHealthBar.transform.localScale.x;
+
+		//RedHealthBar set up
+		RedHealthBar.GetComponent<Renderer> ().material.color = Color.red;
+		redPosition = new Vector3((GreenHealthBar.transform.position.x + GreenHealthBar.transform.localScale.x / 2), GreenHealthBar.transform.position.y, GreenHealthBar.transform.position.z);
+		RedHealthBar.transform.position = redPosition;
+		redCurrentHealth = 0;
+		redOldScaleX = GreenHealthBar.transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (greenMaxHealth != 0) {
+
+			//GreenHealthBar Update
+			greenHealthPercent = ((greenCurrentHealth * 1.0f) / (greenMaxHealth * 1.0f))*100; //Make sure float
+			greenCurrentScale = GreenHealthBar.transform.localScale;
+			greenNewScaleX = greenOldScaleX * greenHealthPercent/100;
+
+			GreenHealthBar.transform.localScale = new Vector3(greenNewScaleX, greenCurrentScale.y, greenCurrentScale.z);
+			GreenHealthBar.transform.Translate((greenNewScaleX - greenCurrentScale.x)/2, 0, 0);
+
+			//RedHealthBar Update
+			redHealthPercent = ((redCurrentHealth * 1.0f) / (redMaxHealth * 1.0f))*100; //Make sure float
+			redCurrentScale = RedHealthBar.transform.localScale;
+			redNewScaleX = redOldScaleX * redHealthPercent/100;
+
+			RedHealthBar.transform.localScale = new Vector3(redNewScaleX, redCurrentScale.y, redCurrentScale.z);
+			RedHealthBar.transform.Translate((redCurrentScale.x - redNewScaleX)/2, 0, 0);
+		}
+	}
+}
