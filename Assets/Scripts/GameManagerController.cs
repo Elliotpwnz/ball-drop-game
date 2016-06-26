6fc69881b9b20b2a455f02bf6af22a8c1c205d5b
Mohammed Jalal Hemidach <Mohammed Jalal Hemidach>
2016-06-26 13:41:16 -0400

    created a Game manager controller script

---
+++ Assets/Scripts/GameManagerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerController : MonoBehaviour {
+	public GameObject[] enemies;
+	private GameObject Archer, Knight, Necromancer, Wizard;
+
+	// Use this for initialization
+	void Start () {
+		Archer = enemies [0];
+		Knight = enemies [1];
+		Necromancer = enemies [2];
+		Wizard = enemies [3];
+
+		Instantiate(Archer, new Vector3(72.0f, 1.0f, 160.0f), Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int rand = Random.Range(0, 5);
+
+
+		//WaitForSeconds (5);
+
+
+	}
+}
